e139e0c86f6f84407c07703f9a7159aae16b80d2
Murad Jafarov <muradkk@code.edu.az>
2022-10-01 19:21:16 +0400

    added C# code

---
+++ Prime and Complex/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Prime_and_Complex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int count = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("n is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("n is a complex number");
+            }
+        }
+    }
+}
